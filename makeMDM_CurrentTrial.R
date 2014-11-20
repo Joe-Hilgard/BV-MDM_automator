68abfa3a759e357125dd92cb778dbd311a97f35d
Joe Hilgard <jhilgard@gmail.com>
2014-11-20 12:01:05 -0600

    Transposed output appropriately, added escaped characters for quotes surrounding .vtc and .sdm files.

---
+++ makeMDM_CurrentTrial.R
@@ -0,0 +1,49 @@
+# NOTE: not sure if this header is appropriate. RFX=0 is particularly troubling,
+  # and NrOfStudies surely has to change.
+setwd("C:/data_2014/Thesis/prt_sdm_automation/BV-MDM_automator")
+# List of all subjects and bolds
+subList = c("WIT001", "WIT002", "WIT003", "WIT004", "WIT005", "WIT006", "WIT007", "WIT008",
+            "WIT009", "WIT010", "WIT011", "WIT012", "WIT013", "WIT014", "WIT015", "WIT016",
+            "WIT017", "WIT018",
+            "WIT101", "WIT102", "WIT103", "WIT104", "WIT105", "WIT106", "WIT107", "WIT108",
+            "WIT109", "WIT110", "WIT111", "WIT112", "WIT113", "WIT114", "WIT115", "WIT116",
+            "WIT117", "WIT118", "WIT119", "WIT120", "WIT121", "WIT122")
+boldList = c("b1", "b2", "b3", "b4", "b5", "b6")
+
+vtcList = c(); sdmList = c()
+for (sub in subList) {
+  for (bold in boldList) {
+    vtcFile = paste("\"/data/BartholowLab/JH_racebias/analysis/",
+                    sub, "/",
+                    sub, "_", bold, "_SCCAI2_3DMCTS_SD3DSS4.00mm_TAL.vtc\"",
+                    sep="")
+    sdmFile = paste("\"/data/BartholowLab/JH_racebias/analysis/SDMs/CurrentTrial/",
+                    sub, "_", bold, "_CurrentTrial.sdm\"",
+                    sep="")
+    vtcList = c(vtcList, vtcFile)
+    sdmList = c(sdmList, sdmFile)
+  }
+}
+
+mdm = matrix(c(vtcList, sdmList), ncol=2, nrow=length(subList)*length(boldList))
+
+# change header NrOfStudies to length(subList)*length(boldList)
+length(subList)*length(boldList)
+header = "
+FileVersion:          3
+TypeOfFunctionalData: VTC
+
+RFX-GLM:              0
+
+PSCTransformation:    0
+zTransformation:      1
+SeparatePredictors:   0
+
+NrOfStudies:          240
+"
+
+# Write that bitch
+cat(header, file="CurrentTrial_240.mdm")
+write(t(mdm), file="CurrentTrial_240.mdm", append=T)
+
+# You'll still need to delete the bad bold/subs by hand so BV doesn't vomit when it hits an NA
