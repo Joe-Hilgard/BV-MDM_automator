6833909e0354d177f5d2ba7c52b29c398e84bc83
Joe-Hilgard <jhilgard@gmail.com>
2014-12-03 18:13:12 -0600

    Added bad-BOLD support to MDM maker.

--- makeMDM_CurrentTrial.R
+++ makeMDM_CurrentTrial.R
@@ -7,12 +7,16 @@ subList = c("WIT001", "WIT002", "WIT003", "WIT004", "WIT005", "WIT006", "WIT007"
             "WIT017", "WIT018",
             "WIT101", "WIT102", "WIT103", "WIT104", "WIT105", "WIT106", "WIT107", "WIT108",
             "WIT109", "WIT110", "WIT111", "WIT112", "WIT113", "WIT114", "WIT115", "WIT116",
-            "WIT117", "WIT118", "WIT119", "WIT120", "WIT121", "WIT122")
+            "WIT117", "WIT118", "WIT119", "WIT120", "WIT121", "WIT123")
 boldList = c("b1", "b2", "b3", "b4", "b5", "b6")
+# read in spreadsheet of bad subject-BOLDs
+bad = read.table(file="list_of_bad_BOLDS.txt")
 
 vtcList = c(); sdmList = c()
 for (sub in subList) {
   for (bold in boldList) {
+    subno = as.numeric(substr(sub, 4, 6)); boldno = as.numeric(substr(bold, 2, 2))
+    if (length(bad$Subject[bad$Subject == subno & bad$Session == boldno]) >0) next
     vtcFile = paste("\"/data/BartholowLab/JH_racebias/analysis/",
                     sub, "/",
                     sub, "_", bold, "_SCCAI2_3DMCTS_SD3DSS4.00mm_TAL.vtc\"",
@@ -25,10 +29,10 @@ for (sub in subList) {
   }
 }
 
-mdm = matrix(c(vtcList, sdmList), ncol=2, nrow=length(subList)*length(boldList))
+mdm = matrix(c(vtcList, sdmList), ncol=2, nrow=length(vtcList))
 
-# change header NrOfStudies to length(subList)*length(boldList)
-length(subList)*length(boldList)
+# change header NrOfStudies to length(vtcList)
+length(vtcList)
 header = "
 FileVersion:          3
 TypeOfFunctionalData: VTC
@@ -39,11 +43,11 @@ PSCTransformation:    0
 zTransformation:      1
 SeparatePredictors:   0
 
-NrOfStudies:          240
+NrOfStudies:          224
 "
 
 # Write that bitch
-cat(header, file="CurrentTrial_240.mdm")
-write(t(mdm), file="CurrentTrial_240.mdm", append=T)
+cat(header, file="CurrentTrial_224.mdm")
+write(t(mdm), file="CurrentTrial_224.mdm", append=T)
 
 # You'll still need to delete the bad bold/subs by hand so BV doesn't vomit when it hits an NA
